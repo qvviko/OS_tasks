91d3881ffe5464c1aeda888343d41e4b2c0e2460
Vladislav <qvviko@gmail.com>
2018-08-29 18:07:33 +0300

    + all tasks

---
+++ week2/ex3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    int n;
+    sscanf(argv[1], "%d", &n);
+    if (n <= 0) {
+        printf("Invalid number. Must be greater then zero\n");
+    } else {
+        char *string = malloc(sizeof(char) * (2 * n * n + 1));
+        for (int j = 1; j <= n; ++j) {
+            int rqr = ((2 * n - 1) - (2 * j - 1)) / 2;
+            int i = (j + 1) * (2 * n);
+            for (i = 0; i < rqr; ++i) {
+                string[i] = ' ';
+            }
+            for (; i < (rqr + 2 * j - 1); ++i) {
+                string[i] = '*';
+            }
+            for (; i < 2 * n - 1; ++i) {
+                string[i] = ' ';
+            }
+            printf("%s\n", string);
+        }
+        free(string);
+    }
+    return 0;
+}
