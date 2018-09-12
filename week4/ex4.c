15966cd10964820406b6af7d76c61a20e1628ae0
Vladislav <qvviko@gmail.com>
2018-09-12 17:30:30 +0300

    +4 task

--- week4/ex4.c
+++ week4/ex4.c
@@ -3,13 +3,13 @@
 #include <unistd.h>
 
 #define TRUE 1
-#define CMD_MAX_LEN 10
+#define CMD_MAX_LEN 1024
 
 int main(void) {
     char cmd[1024];
     while (TRUE) {
         printf("-> ");
         fgets(cmd, CMD_MAX_LEN, stdin);
-        printf("%s", cmd);
+        system(cmd);
     }
 }
