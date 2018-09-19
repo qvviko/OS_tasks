7a109a02dee4417f9d5af2d1da81253d630bda5e
Vladislav <qvviko@gmail.com>
2018-09-19 15:58:02 +0300

    - txt

--- week5/ex2_unfixed.sh
+++ week5/ex2_unfixed.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+rm ex2_unfixed.txt
+1 > ex2_unfixed.txt
 while [ true ]
 do
+# This is critical region because you access the file and then write it
     read_line=$(tail -n 1 ex2.txt)
     read_line=$read_line+1
     echo "$((read_line))" >> ex2.txt
