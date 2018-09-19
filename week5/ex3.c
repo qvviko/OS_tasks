b6dd431c81b358e340dc491f178fa43ce521360d
Vladislav <qvviko@gmail.com>
2018-09-19 20:05:24 +0300

    another e3

--- week5/ex3.c
+++ week5/ex3.c
@@ -1,72 +1,69 @@
 #include <pthread.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+
 
 #define N 8
 #define TRUE 1
+
 int buffer[N];
 pthread_t pr_cons[2];
 int status[2];
-int count = 1;
+int count = 0;
 
 void *producer(void *);
 
 void *consumer(void *);
 
-void *sleep(void);
+void *sleep_thread(void);
 
-void *wakeup(void *);
+void *wakeup_thread(void *);
 
 void *producer(void *id) {
     while (TRUE) {
         if (count == N) {
-            status[0] = 0;
-            printf("Status %d changed to %d, count = %d\n", 0, 0, count);
-            sleep();
+            sleep_thread();
         }
         count++;
         printf("count increased %d\n", count);
-        if (count == 1) wakeup((void *) 1);
+
+        if (count == 1) wakeup_thread((void *) 1);
     }
 }
 
 void *consumer(void *id) {
     while (TRUE) {
         if (count == 0) {
-            status[1] = 0;
-            printf("Status %d changed to %d, count = %d\n", 1, 0, count);
-            sleep();
+            sleep_thread();
         }
         count--;
         printf("count decresed %d\n", count);
-        if (count == N - 1) wakeup((void *) 0);
+
+
+        if (count == N - 1) wakeup_thread((void *) 0);
     }
 }
 
-void *sleep(void) {
-    pthread_exit(NULL);
+void *sleep_thread(void) {
+    printf("Sleep\n");
+    pause();
 }
 
-void *wakeup(void *id) {
-    if ((long) id == 0 && status[0] == 0) {
-        pthread_create(pr_cons, NULL, producer, NULL);
-        status[0] = 1;
-        printf("Status %d changed to %d, count = %d\n", 0, 1, count);
-    } else if (status[1] == 0) {
-        pthread_create(pr_cons + 1, NULL, consumer, NULL);
-        status[1] = 1;
-        printf("Status %d changed to %d, count = %d\n", 1, 1, count);
+void *wakeup_thread(void *id) {
+    if ((long) id == 0) {
+        printf("Continue %d\n", 0);
+        pthread_kill(pr_cons[0], SIGCONT);
+    } else {
+        printf("Continue %d\n", 1);
+        pthread_kill(pr_cons[1], SIGCONT);
     }
 }
 
 int main(void) {
     pthread_create(pr_cons, NULL, producer, NULL);
-    status[0] = 1;
-    printf("Status %d changed to %d, count = %d\n", 0, 1, count);
     pthread_create(pr_cons + 1, NULL, consumer, NULL);
-    status[1] = 1;
-    printf("Status %d changed to %d, count = %d\n", 1, 1, count);
-    while (TRUE) {
-        pthread_join(pr_cons[0], NULL);
-        pthread_join(pr_cons[1], NULL);
-    }
+
+    pthread_join(pr_cons[0], NULL);
+    pthread_join(pr_cons[1], NULL);
 }
