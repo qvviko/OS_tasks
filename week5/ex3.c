55b2236eb5569af0b9d9007012b9570dcc770b79
Vladislav <qvviko@gmail.com>
2018-09-19 20:49:54 +0300

    ex3 done

--- week5/ex3.c
+++ week5/ex3.c
@@ -1,70 +1,73 @@
 #include <pthread.h>
 #include <stdio.h>
-#include <unistd.h>
-#include <signal.h>
-
 
 #define N 8
 #define TRUE 1
 
-int buffer[N];
 pthread_t pr_cons[2];
-int status[2];
+char is_sleeping[2];
 int count = 0;
 
 void *producer(void *);
 
 void *consumer(void *);
 
-void *sleep_thread(void);
+void *sleep_thread(void *);
 
 void *wakeup_thread(void *);
 
 void *producer(void *id) {
     while (TRUE) {
         if (count == N) {
-            sleep_thread();
+            sleep_thread(id);
         }
+
         count++;
-        printf("count increased %d\n", count);
-        if (count == 1) wakeup_thread((void *) 1);
+        printf("Count increased by producer: %d\n", count);
+
+        if (count == 1 && is_sleeping[1 - (long) id] == 1) wakeup_thread((void *) 1);
     }
 }
 
 void *consumer(void *id) {
     while (TRUE) {
         if (count == 0) {
-            sleep_thread();
+            sleep_thread(id);
         }
         count--;
-        printf("count decresed %d\n", count);
+        printf("Count decreased by consumer: %d\n", count);
 
-
-        if (count == N - 1) wakeup_thread((void *) 0);
+        if (count == N - 1 && is_sleeping[1 - (long) id] == 1) wakeup_thread((void *) 0);
     }
 }
 
-void *sleep_thread(void) {
-    printf("Sleep\n");
-    pause();
+void *sleep_thread(void *id) {
+    if ((long) id) {
+        printf("Consumer is sleeping\n");
+    } else {
+        printf("Producer is sleeping\n");
+    }
+    is_sleeping[(long) id] = 1;
+    while (is_sleeping[(long) id] == 1) {
+    }
 }
 
 void *wakeup_thread(void *id) {
-    if ((long) id == 0) {
-        printf("Continue %d\n", 0);
-        printf("%d\n", pthread_kill(pr_cons[0], SIGCONT));
+    if ((long) id) {
+        printf("Consumer awoke\n");
     } else {
-        printf("Continue %d\n", 1);
-        printf("%d\n", pthread_kill(pr_cons[1], SIGCONT));
+        printf("Producer awoke\n");
     }
+    is_sleeping[(long) id] = 0;
 }
 
 int main(void) {
-    pthread_create(pr_cons, NULL, producer, NULL);
-    pthread_create(pr_cons + 1, NULL, consumer, NULL);
-    while (TRUE){
+    pthread_create(pr_cons, NULL, producer, (void *) 0);
+    is_sleeping[0] = 0;
+    pthread_create(pr_cons + 1, NULL, consumer, (void *) 1);
+    is_sleeping[1] = 0;
+
+    while (TRUE) {
         //Wait until race begins...
     }
-//    pthread_join(pr_cons[0], NULL);
-//    pthread_join(pr_cons[1], NULL);
 }
