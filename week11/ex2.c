f76d32dfc0508ba7fd5b81602100ad7c3dcd4eeb
Vladislav <qvviko@gmail.com>
2018-10-31 15:34:00 +0300

    +task 2

---
+++ week11/ex2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(void) {
+    char *hello = "Hello";
+
+    if (setvbuf(stdout, NULL, _IOLBF, 0)) {
+        perror("Error on setbuf");
+        exit(EXIT_FAILURE);
+    }
+    for (int i = 0; i < strlen(hello); ++i) {
+        printf("%c", hello[i]);
+        sleep(1);
+    }
+}
