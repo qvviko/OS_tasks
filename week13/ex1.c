24fdd667df611c81e9cd76c974700fd8b927604d
Vladislav <qvviko@gmail.com>
2018-11-14 14:55:05 +0300

    + task 1

--- week13/ex1.c
+++ week13/ex1.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define UNMARKED 0
+#define MARKED 1
 
 int main(void) {
     FILE *input = fopen("input.txt", "r");
@@ -14,7 +16,6 @@ int main(void) {
     for (int i = 0; i < m; ++i) {
         fscanf(input, "%d", E + i);
     }
-    printf("\n");
     for (int i = 0; i < m; ++i) {
         fscanf(input, "%d", A + i);
     }
@@ -41,5 +42,51 @@ int main(void) {
             fscanf(input, "%d", R[k] + i);
         }
     }
+    char *P = malloc(sizeof(char) * processes_n);
+    for (int n = 0; n < processes_n; ++n) {
+        P[n] = UNMARKED;
+    }
 
+    while (1) {
+        int chosen = -1;
+        for (int i = 0; i < processes_n && chosen == -1; ++i) {
+            fflush(stdout);
+            if (!P[i]) {
+                int all = 1;
+                for (int j = 0; j < m && all; ++j) {
+                    if (A[j] < R[i][j]) {
+                        all = 0;
+                    }
+                }
+                if (all) {
+                    chosen = i;
+                }
+            }
+        }
+        if (chosen != -1) {
+            P[chosen] = MARKED;
+            for (int i = 0; i < m; ++i) {
+                A[i] += C[chosen][i];
+            }
+        } else {
+            break;
+        }
+    }
+    int marked = 1;
+    for (int l = 0; l < processes_n && marked; ++l) {
+        marked = P[l];
+    }
+    if (marked) {
+        FILE *outp = fopen("output_ok", "w+");
+        fprintf(outp, "No deadlock found");
+    } else {
+        FILE *outp = fopen("output_dl", "w+");
+        fprintf(outp, "Deadlock found, processes involved:\n");
+        for (int i = 0; i < processes_n; ++i) {
+            if (!P[i]) {
+                fprintf(outp, "%d ", i + 1);
+            }
+        }
+
+    }
 }
