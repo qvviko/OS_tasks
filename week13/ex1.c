e1e83789f756dd3a3c3de8b3ed9a5835c69c2058
Vladislav <qvviko@gmail.com>
2018-11-14 13:31:57 +0300

    + read arrays

--- week13/ex1.c
+++ week13/ex1.c
@@ -7,41 +7,79 @@ int main(void) {
     FILE *input = fopen("input.txt", "r");
 
     int *E = malloc(sizeof(int) * default_size);
-    int e_size = 0;
-    int e_max = default_size;
-
-    int *A = malloc(sizeof(int) * default_size);
-    int a_size = 0;
-    int a_max = default_size;
+    int m_size = 0;
+    int m_max = default_size;
 
     char temp;
-    while (fscanf(input, "%d%c", E + e_size, &temp)) {
-        e_size++;
+    while (fscanf(input, "%d%c", E + m_size, &temp)) {
+        m_size++;
         if (temp == '\n') {
             break;
         }
-        if (e_size >= e_max) {
-            e_max += default_size;
-            E = realloc(E, e_max * sizeof(int));
+        if (m_size >= m_max) {
+            m_max += default_size;
+            E = realloc(E, m_max * sizeof(int));
         }
     }
-
-    while (fscanf(input, "%d%c", A + a_size, &temp)) {
-        a_size++;
+    int *A = malloc(sizeof(int) * m_size);
+    int i = 0;
+    while (fscanf(input, "%d%c", A + i, &temp)) {
+        i++;
         if (temp == '\n') {
             break;
         }
-        if (a_size >= a_max) {
-            a_max += default_size;
-            E = realloc(E, a_max * sizeof(int));
+    }
+
+    int processes_n = 0;
+    int max_processes = default_size;
+
+    int **C = malloc(sizeof(int *) * max_processes);
+
+    for (int j = 0; j < max_processes; ++j) {
+        C[j] = malloc(sizeof(int *) * m_size);
+    }
+
+    int **R = malloc(sizeof(int *) * max_processes);
+    for (int j = 0; j < max_processes; ++j) {
+        C[j] = malloc(sizeof(int *) * m_size);
+    }
+
+
+    char *buffer = malloc(sizeof(char) * (3 * m_size));
+    while (fgets(buffer, 3 * m_size, input)) {
+        printf("%s", buffer);
+        if (buffer == "\n") {
+            break;
+        } else {
+            i = 0;
+            while (sscanf(buffer, "%d", C[processes_n] + i)) {
+                i++;
+            }
+        }
+        processes_n++;
+        if (processes_n == max_processes) {
+            max_processes += default_size;
+            C = realloc(C, sizeof(int *) * max_processes);
+
+            for (int j = max_processes - default_size; j < max_processes; ++j) {
+                C[j] = malloc(sizeof(int *) * m_size);
+            }
         }
     }
 
-    int **C =;
+    while (fgets(buffer, 3 * m_size, input)) {
+        i = 0;
+        while (sscanf(buffer, "%d", C[processes_n] + i)) {
+            i++;
+        }
+        processes_n++;
+        if (processes_n == max_processes) {
+            max_processes += default_size;
+            C = realloc(C, sizeof(int *) * max_processes);
 
-    int **R =;
-
-    for (int i = 0; i < e_size; ++i) {
-        printf("%d ", A[i]);
+            for (int j = max_processes - default_size; j < max_processes; ++j) {
+                C[j] = malloc(sizeof(int *) * m_size);
+            }
+        }
     }
 }
